6d5f676e3b070003bf2775cf9b3b0abb19d7c458
nublius <184036331+nublius@users.noreply.github.com>
2025-05-25 21:22:28 -0700

    add getAvailableTokens function in Player class

--- scripts.js
+++ scripts.js
@@ -81,11 +81,22 @@ class Player {
     }
 
     initTokens(value) {
+        this.tokenArray = [];
         for(let i = 0; i < 7; i++) {
             const id = `${value}${i + 1}`;
             const newToken = new Token(this.name, id);
             this.tokenArray.push(newToken);
         }
+    }
+
+    resetTokens() {
+        for(let i = 0; i < this.tokenArray.length; i++) {
+            this.tokenArray[i].reset();
+        }
+    }
+
+    getAvailableTokens() {
+        return this.tokenArray.filter(token => token.isOnBoard && !token._hasExited);
     }
 }
 
